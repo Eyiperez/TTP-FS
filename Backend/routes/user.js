eea03d9cc17a09bf70af9488332e77593be15982
Erika Perez <erikaperez@Erikas-Air.fios-router.home>
2019-07-03 00:01:15 -0400

    corrected type error

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -2,9 +2,28 @@ const express = require('express');
 const userRouter = express.Router();
 const { UserService } = require('../services/user');
 
+const { isRequiredsNeededUser} = require('../services/utils')
+
 
 //POST- CREATE USER
-
+sellerRouter.post('/', (req, res, next) => {
+    const { name, email, user_uid, user_photo } = req.body;
+    if (isRequiredsNeededUser(req.body)) {
+      res.status(400)
+      res.send({
+        "msg": "some required values are missing",
+      })
+    }
+    UserService.create(name, email, user_uid, user_photo)
+      .then(data => {
+        res.status(200)
+        res.json({ success: `Created user named ${name} with generated ID: ${data.id}`, id: data.id });
+      })
+      .catch(err => {
+        res.status(400)
+        next(err);
+      })
+  });
 
 
 module.exports = {userRouter,};
