fbb72ed8197e29cd71775bd5291ec848be9be8dd
Erika Perez <erikaperez@Erikas-Air.fios-router.home>
2019-07-03 00:04:29 -0400

    created post route for user

--- Backend/services/user.js
+++ Backend/services/user.js
@@ -1,7 +1,11 @@
 const { db } = require('./pgp');
 const UserService = {};
 
-
+//CREAT USER
+UserService.create = (name, email, user_uid, user_photo) => {
+    const sql = `INSERT INTO users (name, email, user_uid, user_photo) VALUES ($[name], $[email], $[user_uid], $[user_photo]) RETURNING id;`;
+    return db.one(sql, { name, email, user_uid, user_photo });
+  }
 
 
 module.exports = { UserService, };
