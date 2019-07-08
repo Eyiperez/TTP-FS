f2da0feaa15ef88b80a1e51a33ae4b718e45e295
Erika Perez <erikaperez@Erikas-Air.fios-router.home>
2019-07-08 13:53:50 -0400

    axios call to get users stocks on component did mount

--- Frontend/ttp-frontend/src/containers/portfolio.js
+++ Frontend/ttp-frontend/src/containers/portfolio.js
@@ -3,42 +3,72 @@ import { withRouter } from 'react-router-dom';
 import AuthContext from '../contexts/auth';
 import UserNavs from '../components/userNavs';
 import UserMeida from '../components/userMedia';
+import axios from 'axios';
 
 import '../styles/app.css';
 
 import { Container, Row, Col } from 'reactstrap';
 
+
 class Portfolio extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      userStocks: [],
+      userIEXData: [],
+      portfolioValue: 0,
+      availableCash: 0,
+      tickerName: '',
+      newTicker: {},
+      error: ''
+    }
+  }
+
+  componentDidMount() {
+    const user_id = this.props.match.params.user_id;
+    const getStocksUrl = `http://localhost:3001/stocks/1`;
+    axios.get(getStocksUrl)
+      .then((data) => {
+        const userData = data.data;
+        const stocks = userData.stocks;
+        const iexData = userData.iexData;
+        this.setState({ userStocks: stocks, userIEXData: iexData })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
 
   render() {
 
     return <AuthContext.Consumer>
-        {
-          (user) => {
-            if (user) {
-              return <Container className='container'>
-                <UserMeida userEmail={user.email}></UserMeida>
-                <UserNavs userEmail={user.email}></UserNavs>
-                <Row>
-                  <Col>
+      {
+        (user) => {
+          if (user) {
+            return <Container className='container'>
+              <UserMeida userEmail={user.email}></UserMeida>
+              <UserNavs userEmail={user.email}></UserNavs>
+              <Row>
+                <Col>
                   <h1>Portfolio</h1>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
                   <h1>List</h1>
-                  </Col>
-                  <Col>
+                </Col>
+                <Col>
                   <h1>Cash</h1>
-                  </Col>
-                </Row>
-              </Container>
-            } else {
-              return <h1>Please login</h1>
-            }
+                </Col>
+              </Row>
+            </Container>
+          } else {
+            return <h1>Please login</h1>
           }
         }
-      </AuthContext.Consumer>
+      }
+    </AuthContext.Consumer>
   }
 }
 
