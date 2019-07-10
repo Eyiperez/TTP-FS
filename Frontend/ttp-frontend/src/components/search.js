92f2a4c412c4e0f7476a928d83e5b1772fa3509b
Erika Perez <erikaperez@Erikas-MacBook-Air.local>
2019-07-09 21:20:03 -0400

    installed react-moment to get date

---
+++ Frontend/ttp-frontend/src/components/search.js
@@ -0,0 +1,30 @@
+import React from 'react';
+//import axios from 'axios';
+
+
+class Search extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: '',
+            user_id: null,
+            ticker_symbol: '',
+            stock_name: '',
+            qty_owned: '',
+            price: '',
+            type: '',
+            date: ''
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return
+
+    }
+}
+
+export default (Search);
